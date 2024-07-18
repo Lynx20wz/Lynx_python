b1680d4c3d5511596c0647e74634f39f98925dd7
Lynx20wz <167802307+Lynx20wz@users.noreply.github.com>
2024-07-18 22:06:04 +0300

    много чего

---
+++ hoi manager/get_name_with_help_id.py
@@ -0,0 +1,32 @@
+import json
+from loguru import logger
+
+logger.remove()
+logger.add(
+    sink='logs/get_name.log',
+    level='INFO',
+    mode='w',
+    format='{time:H:mm:ss} | <level>{level}</level> | {message}',
+)
+def bootstrap_id():
+    global tag_countries
+    try:
+        with open('tag_countries.json', 'r', encoding='UTF_8') as file_json:
+            tag_countries = json.loads(file_json.read())
+            logger.info(f'Подгружены id стран')
+    except:
+        logger.critical('не удалось загрузить id стран')
+
+def get_name_with_help_id(name: str) -> str:
+    if len(name) == 3:
+        try:
+            logger.info(f'{name} - {tag_countries.get(name)[0]}')
+            return tag_countries.get(name)[0]
+        except TypeError:
+            pass
+    for k, s in tag_countries.items():
+        for s in s:
+            if s == name:
+                logger.info(f'{name} - {k}')
+                return k
+bootstrap_id()
